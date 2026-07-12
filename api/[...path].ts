19c58f4621f7ee5fdb2715e6dc9e76dce5d7f465
AN-SWAPNIL <a.n.swapnil2003@gmail.com>
2026-07-12 12:37:21 +0600

    Route mock API through single Vercel function

--- api/[...path].ts
+++
@@ -1,5 +0,0 @@
-import app from "../server/index.js";
-
-export default function handler(req: any, res: any) {
-  return app(req, res);
-}
--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,25 @@
 import app from "../server/index.js";
 
-export default function handler(req: any, res: any) {
-  return app(req, res);
+type VercelLikeRequest = {
+  url?: string;
+  headers: Record<string, string | string[] | undefined>;
+  query?: Record<string, string | string[] | undefined>;
+};
+
+function firstQueryValue(value: string | string[] | undefined) {
+  return Array.isArray(value) ? value[0] : value;
+}
+
+export default function handler(req: VercelLikeRequest, res: any) {
+  const rewrittenPath = firstQueryValue(req.query?.path);
+
+  if (rewrittenPath) {
+    const host = firstQueryValue(req.headers.host) ?? "localhost";
+    const url = new URL(req.url ?? "/", `https://${host}`);
+    url.searchParams.delete("path");
+    const query = url.searchParams.toString();
+    req.url = `/api/${rewrittenPath}${query ? `?${query}` : ""}`;
+  }
+
+  return app(req as any, res);
 }
